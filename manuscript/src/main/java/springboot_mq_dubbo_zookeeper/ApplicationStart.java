57cd97f04dbab0e109841fcaa5baaf799d91b07e
leiguodong <461460477@qq.com>
2021-05-19 17:53:55 +0800

    面试聊什么

---
+++ manuscript/src/main/java/springboot_mq_dubbo_zookeeper/ApplicationStart.java
@@ -0,0 +1,10 @@
+package springboot_mq_dubbo_zookeeper;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lei on 2021/5/19.
+ */
+@SpringBootApplication
+public class ApplicationStart {
+}
